f1aca9caa836a650715fa0e0432892c7388acc6c
koval2001 <48998783+koval2001@users.noreply.github.com>
2019-10-07 23:58:46 +0300

    first_classes

---
+++ MyClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace paroplav
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			//Reservoir res = new Reservoir(1050, 0, 100, 50000);
+			Reservoir res = new Reservoir();
+			Console.Write("введiть площу водоймища(м.кв.) ");
+			res.Square = int.Parse(Console.ReadLine());
+			Console.Write("введiть кiлькiсть впадаючих рiчок ");
+			res.Number = int.Parse(Console.ReadLine());
+			Console.Write("введiть найбiльшу глибину(м.) ");
+			res.MaxDepth = double.Parse(Console.ReadLine());
+			Console.Write("введiть масу води(кг.) ");
+			res.WeightWater = double.Parse(Console.ReadLine());
+
+
+			Console.WriteLine("");
+			Console.WriteLine("середня глибина водоймища = " + res.AverageDepth() + "(м.)");
+			if (res.WaterAnalise())
+				Console.WriteLine("маса води бiльша нiж 1000(кг.)");
+			else
+				Console.WriteLine("маса води менша нiж 1000(кг.)");
+
+			Console.WriteLine(res.NumberRivers());
+
+			// Lake
+			//Console.WriteLine("Клас Lake");
+			//var lake = new Lake();
+			//Console.Write("введiть кiлькiсть причалiв ");
+			//lake.BerthsQty = int.Parse(Console.ReadLine());
+
+			//Pond
+			//var pond = new Pond();
+
+
+
+			Console.ReadLine();
+		}
+	}
+}
---
+++ paroplav/Reservoir.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace paroplav
+{
+    public class Reservoir
+    {
+		protected int square;//площадь
+		protected int number;//количество
+		protected double maxDepth;//найбольшая глубина
+		protected double weightWater;//масса воды
+
+		public Reservoir()
+		{ }
+
+		//конструктор класса
+		public Reservoir(int square, int number, double maxDepth, double weightWater)
+		{
+			this.square = square;
+			this.number = number;
+			this.maxDepth = maxDepth;
+			this.weightWater = weightWater;
+		}
+
+		//методы класса
+		public virtual double AverageDepth()//вычисление средней глубины
+		{
+			return this.maxDepth / 2;
+		}
+
+		/// <summary>
+		/// масса воды меньше 1000?
+		/// </summary>
+		/// <returns></returns>
+		public bool WaterAnalise()
+		{
+			if (weightWater < 1000)
+				return false;
+			else
+				return true;
+		}
+
+		/// <summary>
+		/// количество речек, что впадают
+		/// </summary>      
+		public string NumberRivers()
+		{
+			if (number <= 0)
+				return "у це водоймище рiчки не впадають";
+			else
+				return "кiлькiсть рiчок, що впадає у це водоймище = " + number.ToString();
+		}
+
+		//свойства класса
+		public double MaxDepth
+		{
+			get { return this.maxDepth; }
+			set { this.maxDepth = value; }
+		}
+		public int Square
+		{
+			get { return this.square; }
+			set { this.square = value; }
+		}
+		public int Number
+		{
+			get { return this.number; }
+			set { this.number = value; }
+		}
+		public double WeightWater
+		{
+			get { return this.weightWater; }
+			set { this.weightWater = value; }
+		}
+
+        public override float PartOfTheyearWhenThereIsNoIce()
+        {
+            return new TimeSpan(365, 0, 0, 0).Milliseconds / (float)IceingTime.Milliseconds;
+        }
+	}
+}
